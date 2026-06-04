51573e08c882f66b14eac4483a61d8cd7fe06540
Muhammad <faisal@Faisals-MacBook-Pro.local>
2026-06-04 20:45:50 +0530

    Updated gitignore for generated reports

--- masterclean/reader.py
+++ masterclean/reader.py
@@ -1,10 +1,34 @@
 import pandas as pd
+import chardet
+
+
+def detect_encoding(file_path):
+
+    with open(file_path, "rb") as file:
+
+        raw_data = file.read(100000)
+
+    result = chardet.detect(raw_data)
+
+    encoding = result["encoding"]
+
+    return encoding
+
 
 def read_file(file_path):
 
     try:
 
-        df = pd.read_csv(file_path)
+        # Detect encoding
+        encoding = detect_encoding(file_path)
+
+        print(f"📄 Detected Encoding: {encoding}")
+
+        # Read CSV
+        df = pd.read_csv(
+            file_path,
+            encoding=encoding
+        )
 
         print("✅ CSV loaded successfully")
 
